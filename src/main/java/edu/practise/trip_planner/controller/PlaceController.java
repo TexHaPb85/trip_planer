2cc244ff88ccd5837e21245eb65e11465df35974
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-09-08 15:24:54 +0300

    comments removing implemented

--- src/main/java/edu/practise/trip_planner/controller/PlaceController.java
+++ src/main/java/edu/practise/trip_planner/controller/PlaceController.java
@@ -20,20 +20,20 @@ public class PlaceController {
     }
 
     @GetMapping
-    public List<TourPlace> findAllPlaces(){
+    public List<TourPlace> findAllPlaces() {
         return placeService.findAllPlaces();
     }
 
     @PostMapping
     @ResponseBody
-    public ResponseEntity addPlace(@RequestBody TourPlace tourPlace){
+    public ResponseEntity addPlace(@RequestBody TourPlace tourPlace) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(placeService.addPlace(tourPlace));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity deletePlaceById(@PathVariable("id") Long id ){
+    public ResponseEntity deletePlaceById(@PathVariable("id") Long id) {
         placeService.deletePlaceById(id);
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -41,7 +41,7 @@ public class PlaceController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<TourPlace> findPlaceById(@PathVariable("id") Long id){
+    public ResponseEntity<TourPlace> findPlaceById(@PathVariable("id") Long id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(placeService.findPlaceById(id));
@@ -49,21 +49,30 @@ public class PlaceController {
 
     @PutMapping("{id}")
     @ResponseBody
-    public ResponseEntity editPlaceById(@PathVariable("id") Long id, @RequestBody TourPlace tourPlace){
+    public ResponseEntity editPlaceById(@PathVariable("id") Long id, @RequestBody TourPlace tourPlace) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("The tourPlace edited" + placeService.editPlaceById(id, tourPlace));
     }
 
     @PostMapping("{id}/comments")
-    public ResponseEntity addCommentAboutPlace(@PathVariable("id") Long placeID, @RequestBody Comment comment){
+    public ResponseEntity addCommentAboutPlace(@PathVariable("id") Long placeID, @RequestBody Comment comment) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(placeService.addCommentToPlace(placeID,comment));
+                .body(placeService.addCommentToPlace(placeID, comment));
+    }
+
+    @DeleteMapping("{placeId}/comments/{commentForDeleteId}")
+    public ResponseEntity deleteCommentById(@PathVariable("placeId") Long placeId,
+                                            @PathVariable("commentForDeleteId") Long commentForDeleteId) {
+        placeService.deleteCommentById(commentForDeleteId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("The comment to place " + placeId + " with id " + commentForDeleteId + "was removed");
     }
 
     @GetMapping("{id}/comments")
-    public ResponseEntity getCommentsById(@PathVariable("id") Long id){
+    public ResponseEntity getCommentsById(@PathVariable("id") Long id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(placeService.findCommentsByPlaceId(id));
